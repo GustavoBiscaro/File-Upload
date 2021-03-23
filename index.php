c5a0ec0b28c47054fbb3af2a3ac6088661587854
Gustavo Miguel <gustavo.biscaro@gmail.com>
2021-03-22 21:41:08 -0300

    News files index.php and recebe.php

---
+++ index.php
@@ -0,0 +1,5 @@
+﻿<form method="post" enctype="multipart/form-data" action="recebe.php">
+    <input type="file" name="arquivo"><br/><br/>
+
+    <input type="submit" value="Enviar">
+</form>
---
+++ recebe.php
@@ -0,0 +1,3 @@
+﻿<?php
+    $arquivo = $_FILES['arquivo'];
+?>
